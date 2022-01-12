a0da3eeb8e7b837b63f198cd2354bfc548ecd4f0
globotix <denesh.siva@globotix.sg>
2022-01-13 00:57:42 +0800

    Adding redis search to the app

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,12 @@
 import CarForm from "../lib/CarForm";
+import SearchForm from "../lib/SearchForm";
 
 export default function Home() {
   return (
     <div>
       <h1>Create a car</h1>
+      <SearchForm />
+      <br></br>
       <CarForm />
     </div>
   )
